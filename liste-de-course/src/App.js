df63d784b3d272ce2e8df4b6226d7cc5a7bb9c48
degreffet <denis.greffet@etu.uca.fr>
2022-03-29 00:05:20 +0200

    ajout react-icons, ajout style

--- liste-de-course/src/App.js
+++ liste-de-course/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Items from './components/Items';
+import { useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([
+    {
+        "id": 1,
+        "name": "Carotte",
+        "category": "aliment",
+        "url": "https://sweetnitro.com/public/uploads/avatars/5030947003039744.jpg",
+        "quantityAlreadyHave": 0,
+        "quantityNeeded": 0,
+        "wanted": true
+    },
+    {
+      "id": 1,
+      "name": "Carotte",
+      "category": "aliment",
+      "url": "https://sweetnitro.com/public/uploads/avatars/5030947003039744.jpg",
+      "quantityAlreadyHave": 0,
+      "quantityNeeded": 0,
+      "wanted": true
+  },
+  {
+    "id": 1,
+    "name": "Carotte",
+    "category": "aliment",
+    "url": "https://sweetnitro.com/public/uploads/avatars/5030947003039744.jpg",
+    "quantityAlreadyHave": 0,
+    "quantityNeeded": 0,
+    "wanted": true
+  },
+  {
+    "id": 1,
+    "name": "Carotte",
+    "category": "aliment",
+    "url": "https://sweetnitro.com/public/uploads/avatars/5030947003039744.jpg",
+    "quantityAlreadyHave": 0,
+    "quantityNeeded": 0,
+    "wanted": true
+  },
+  {
+    "id": 1,
+    "name": "Carotte",
+    "category": "aliment",
+    "url": "https://sweetnitro.com/public/uploads/avatars/5030947003039744.jpg",
+    "quantityAlreadyHave": 0,
+    "quantityNeeded": 0,
+    "wanted": false
+  },
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Items items={items}/>
     </div>
   );
 }
